4f42c649758a4668716fd88822afd95d347fa2d3
Chongsun Ahn <cahn922@yahoo.com>
2021-07-02 16:07:01 -0700

    Edit jenkins shell to use docker directly
    For some reason Jenkins and docker-compose on the build server do not work well together. Workaround until this is resolved.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-docker-compose -f docker-compose.builder.yml run -e BUILD_NUMBER=$1 -e GIT_BRANCH=$2 builder
-docker-compose -f docker-compose.builder.yml build image
-docker-compose -f docker-compose.builder.yml down --volumes
+docker run --name esw-fulfillment-extensions -v '/var/lib/jenkins/workspace/esw-fulfillment-extensions:/app' openlmis/dev:6 ./build.sh
+docker rm -f esw-fulfillment-extensions
